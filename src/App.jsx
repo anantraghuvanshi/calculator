14243de8671a931a3e86e4e34a0fcf757ea34fa7
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-21 02:15:21 +0530

    button component add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
   return (
     <div className={styles.calculator}>
       <Display />
-      <div id="buttons-container">
-        <button>C</button>
-      </div>
+      <ButtonsContainer />
     </div>
   );
 }
