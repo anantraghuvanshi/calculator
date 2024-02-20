c96b249b2c54b407412f596e79dcff06fbfb3fba
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-21 01:39:25 +0530

    css modules add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import styles from "./App.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <div id="calculator">
+      <input type="text" id="display" />
+      <div id="buttons-container">
+        <button>C</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
