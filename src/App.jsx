1856e5b8134da6f44411811b2237c3f265de0cea
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-02 00:29:56 +0530

    using states

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("0");
+  const onButtonClick = (buttonText) => {};
+
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonsContainer />
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick} />
     </div>
   );
 }
